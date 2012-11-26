c3751342f783ff96551303b510bcb646b3cf6299
Bart Kiers <bkiers@gmail.com>
2012-11-26 22:24:53 +0100

    Added some parser unit tests.

--- src/test/pcreparser/PCREParserTest.java
+++ src/test/pcreparser/PCREParserTest.java
@@ -1,5 +1,6 @@
 package pcreparser;
 
+import com.sun.istack.internal.NotNull;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
@@ -7,6 +8,7 @@ import org.antlr.runtime.tree.CommonTree;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.*;
 
@@ -142,66 +144,207 @@ public class PCREParserTest {
 
     @Test
     public void cc_atomTest() throws Exception {
-        // TODO
+
+        String[] ranges = {"0-9", "\\]-$", "---", "[-["};
+
+        for(String range : ranges) {
+
+            PCREParser parser = getParser(range);
+            PCREParser.cc_atom_return value = parser.cc_atom();
+
+            assertThat(value, notNullValue());
+
+            CommonTree tree = (CommonTree)value.getTree();
+
+            assertThat(tree.getChildCount(), is(2));
+            assertThat(tree.getType(), is(PCRELexer.RANGE));
+        }
+
+        String[] tests = (
+                "a b c [[:digit:]] \\d * $ ?"
+        ).split("\\s+");
+
+
+        for(String test : tests) {
+
+            PCREParser parser = getParser(test);
+            PCREParser.cc_atom_return value = parser.cc_atom();
+
+            assertThat(value, notNullValue());
+        }
     }
 
     @Test
     public void shared_atomTest() throws Exception {
-        // TODO
+
+        String[] tests = (
+                "[[:digit:]] [[:^upper:]] \\cX \\d \\D \\h \\H \\N " +
+                "\\p{Thai} \\P{Thai} \\R \\s \\S \\v \\V \\w \\W"
+        ).split("\\s+");
+
+
+        for(String test : tests) {
+
+            PCREParser parser = getParser(test);
+            PCREParser.shared_atom_return value = parser.shared_atom();
+
+            assertThat(value, notNullValue());
+        }
     }
 
     @Test
     public void literalTest() throws Exception {
-        // TODO
+
+        PCREParser parser = getParser("]");
+        PCREParser.literal_return value = parser.literal();
+
+        assertThat(value, notNullValue());
+
+        CommonTree tree = (CommonTree)value.getTree();
+
+        assertThat(tree.getChildCount(), is(0));
+        assertThat(tree.getType(), is(PCRELexer.LITERAL));
     }
 
     @Test
     public void cc_literalTest() throws Exception {
-        // TODO
+
+        String[] tests = ". [ ^ ? + * \\b $ | ( )".split("\\s+");
+
+        for(String test : tests) {
+
+            PCREParser parser = getParser(test);
+            PCREParser.cc_literal_return value = parser.cc_literal();
+
+            assertThat(value, notNullValue());
+
+            CommonTree tree = (CommonTree)value.getTree();
+
+            assertThat(tree.getChildCount(), is(0));
+            assertThat(tree.getType(), is(PCRELexer.LITERAL));
+        }
     }
 
     @Test
     public void shared_literalTest() throws Exception {
-        // TODO
+
+        String[] tests = (
+                "a 4 \\a \\e \\f \\n \\r \\t \\xFF \\x{1234} \\. " +
+                "\\Q...\\E { } , - < > ' : _ : # = ! & ~ ` @ %"
+        ).split("\\s+");
+
+
+        for(String test : tests) {
+
+            PCREParser parser = getParser(test);
+            PCREParser.shared_literal_return value = parser.shared_literal();
+
+            assertThat(value, notNullValue());
+
+            CommonTree tree = (CommonTree)value.getTree();
+
+            assertThat(tree.getChildCount(), is(0));
+            assertThat(tree.getType(), is(PCRELexer.LITERAL));
+        }
     }
 
     @Test
     public void numberTest() throws Exception {
-        // TODO
+
+        String source = "4567";
+        PCREParser parser = getParser(source + " ... ");
+
+        PCREParser.number_return value = parser.number();
+        CommonTree tree = (CommonTree)value.getTree();
+
+        assertThat(tree.getChildCount(), is(0));
+        assertThat(tree.getType(), is(PCRELexer.NUMBER));
+        assertThat(tree.getText(), is(source));
     }
 
     @Test
     public void digitsTest() throws Exception {
-        // TODO
+
+        String source = "0123456789";
+        PCREParser parser = getParser(source);
+
+        PCREParser.digits_return value = parser.digits();
+        CommonTree tree = (CommonTree)value.getTree();
+
+        assertThat(tree.getChildCount(), is(10));
     }
 
     @Test
     public void digitTest() throws Exception {
-        // TODO
+
+        String source = "0123456789";
+        PCREParser parser = getParser(source);
+
+        for(int i = 0; i < source.length(); i++) {
+            PCREParser.digit_return value = parser.digit();
+            assertThat(value, notNullValue());
+        }
     }
 
     @Test
     public void nameTest() throws Exception {
-        // TODO
+
+        String source = "justAname";
+        PCREParser parser = getParser(source + " ... ");
+
+        PCREParser.name_return value = parser.name();
+        CommonTree tree = (CommonTree)value.getTree();
+
+        assertThat(tree.getChildCount(), is(0));
+        assertThat(tree.getType(), is(PCRELexer.NAME));
+        assertThat(tree.getText(), is(source));
     }
 
     @Test
     public void lettersTest() throws Exception {
-        // TODO
+
+        String source = "abc)";
+        PCREParser parser = getParser(source);
+
+        PCREParser.letters_return value = parser.letters();
+        CommonTree tree = (CommonTree)value.getTree();
+
+        assertThat(tree.getChildCount(), is(3));
     }
 
     @Test
     public void non_close_parensTest() throws Exception {
-        // TODO
+
+        String source = "abc)";
+        PCREParser parser = getParser(source);
+
+        PCREParser.non_close_parens_return value = parser.non_close_parens();
+        CommonTree tree = (CommonTree)value.getTree();
+
+        assertThat(tree.getChildCount(), is(3));
     }
 
     @Test
     public void non_close_parenTest() throws Exception {
-        // TODO
+
+        String source = "abc)";
+        PCREParser parser = getParser(source);
+
+        for(int i = 0; i < source.length() - 1; i++) {
+            PCREParser.non_close_paren_return value = parser.non_close_paren();
+            assertThat(value, notNullValue());
+        }
     }
 
     @Test
     public void letterTest() throws Exception {
-        // TODO
+
+        String source = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        PCREParser parser = getParser(source);
+
+        for(int i = 0; i < source.length(); i++) {
+            PCREParser.letter_return value = parser.letter();
+            assertThat(value, notNullValue());
+        }
     }
 }
